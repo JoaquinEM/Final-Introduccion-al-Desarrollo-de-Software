ebb08902daa3975d8ca17cf1aff49fcf9e3c68f9
Juan Torga <jtorga@fi.uba.ar>
2024-06-20 01:52:55 -0300

    el juego ya funciona y se anuncia en consola el ganador

--- public/juego/juego.js
+++ public/juego/juego.js
@@ -1,5 +1,21 @@
+function agregarO(){
+    let posicion = Math.floor(Math.random() * 8)
+    let casillas = document.getElementsByName("equis")
+    if (casillas[posicion].getAttribute("value") == "Cerrado"){
+        agregarO()
+        return
+    }
+    for (let casilla = 0; casilla < 9; casilla++){
+        if (casillas[casilla].getAttribute("value") == "Disponible" && casillas[casilla].getAttribute("id") == posicion){
+            casillas[casilla].innerText = "O"
+            casillas[casilla].setAttribute("value","Cerrado")
+        }
+    }
+}
+
 function agregarX(id){
     let casilla = document.getElementById(`${id}`)
-    casilla.innerText = "X";
-    console.log(casilla)
+    casilla.innerText = "X"
+    casilla.setAttribute("value", "Cerrado")
+    agregarO()
 }
