4158ef41667fdbe799fa9a0f0647105204b7d12f
Joaquin Enriquez Marin <162145039+JoaquinEM@users.noreply.github.com>
2024-06-21 10:50:15 -0300

    Agrego variables globales al JS de juego creo la funcion inicializacionJuego()

--- public/juego/juego.js
+++ public/juego/juego.js
@@ -1,36 +1,54 @@
-function comprobador(xRecta,ganador,arranque,suma){
+
+let CRUZ = "X"
+let CIRCULO = "O"
+let VACIO = "-"
+let estado_inicial = "disponible"
+let estado_final = "ocupado"
+
+function comprobador(tablero,ganador,arranque,suma){
     segundaPosicion = arranque + suma
-    if (xRecta[arranque] == xRecta[arranque+suma] && xRecta[arranque+suma] == xRecta[segundaPosicion+suma] && xRecta[segundaPosicion+suma] == `${ganador}`){
+    if (tablero[arranque] == tablero[arranque+suma] && tablero[arranque+suma] == tablero[segundaPosicion+suma] && tablero[segundaPosicion+suma] == `${ganador}`){
         console.log(`GANÓ: ${ganador}`)
     }
 }
 
-function comprobarGanador(ganador){
-    let xRecta = ["*","*","*","*","*","*","*","*","*"]
-    casilla = document.getElementsByName("equis")
-    for (let posicion = 0; posicion < 9; posicion++){
-        if (casilla[posicion].innerText == "X"){
-            xRecta[posicion] = "x"
-        }
-        if (casilla[posicion].innerText == "O"){
-            xRecta[posicion] = "o"
-        }
+function inicializacionJuego(){
+    let tablero = new Array(9)
+    for (let i = 0; i < tablero.length; i++){
+        tablero[i] = VACIO
     }
-    comprobador(xRecta,ganador,0,1)
-    comprobador(xRecta,ganador,3,1)
-    comprobador(xRecta,ganador,6,1)
-
-    comprobador(xRecta,ganador,0,3)
-    comprobador(xRecta,ganador,1,3)
-    comprobador(xRecta,ganador,2,3)
-
-    comprobador(xRecta,ganador,0,4)
+    return tablero
 }
 
-function agregarO(veces){
+function comprobarGanador(ganador){
+    let tablero = inicializacionJuego()
+
+    casilla = document.getElementsByName("equis")
+    
+    for (let posicion = 0; posicion < 9; posicion++){
+        if (casilla[posicion].innerText == CRUZ){
+            tablero[posicion] = CRUZ
+        }
+        if (casilla[posicion].innerText == CIRCULO){
+            tablero[posicion] = CIRCULO
+        }
+    }
+    comprobador(tablero,ganador,0,1)
+    comprobador(tablero,ganador,3,1)
+    comprobador(tablero,ganador,6,1)
+
+    comprobador(tablero,ganador,0,3)
+    comprobador(tablero,ganador,1,3)
+    comprobador(tablero,ganador,2,3)
+
+    comprobador(tablero,ganador,0,4)
+}
+
+function agregarCirculo(veces){
     let posicion = Math.floor(Math.random() * 8)
     let casillas = document.getElementsByName("equis")
-    if (casillas[posicion].getAttribute("value") == "Cerrado"){
+    
+    if (casillas[posicion].getAttribute("value") == estado_final){
         veces += 1
         if (veces == 8){
             return
@@ -39,25 +57,25 @@ function agregarO(veces){
         return
     }
     for (let casilla = 0; casilla < 9; casilla++){
-        if (casillas[casilla].getAttribute("value") == "Disponible" && casillas[casilla].getAttribute("id") == posicion){
-            casillas[casilla].innerText = "O"
-            casillas[casilla].setAttribute("value","Cerrado")
+        if (casillas[casilla].getAttribute("value") == estado_inicial && casillas[casilla].getAttribute("id") == posicion){
+            casillas[casilla].innerText = CIRCULO
+            casillas[casilla].setAttribute("value",estado_final)
         }
     }
 }
 
-function agregarX(id){
+function agregarCruz(id){
     let casilla = document.getElementById(`${id}`)
     let estado_casilla = casilla.getAttribute('value')
 
 
-    if (estado_casilla !== "Cerrado") {
-        casilla.innerText = "X"
-        casilla.setAttribute("value", "Cerrado")
+    if (estado_casilla !== estado_final) {
+        casilla.innerText = CRUZ
+        casilla.setAttribute("value", estado_final)
 
-        agregarO(0)
-        comprobarGanador("x")
-        comprobarGanador("o")
+        agregarCirculo(0)
+        comprobarGanador(CRUZ)
+        comprobarGanador(CIRCULO)
     }
     
 
