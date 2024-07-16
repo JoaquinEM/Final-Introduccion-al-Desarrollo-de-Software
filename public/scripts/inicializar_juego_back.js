5451c0dc7beb5e5b2506dc862210441bd147b86e
Juan Torga <jtorga@fi.uba.ar>
2024-07-16 13:38:48 -0300

    Detalles de funciones y metodos comentados

--- public/scripts/inicializar_juego_back.js
+++ public/scripts/inicializar_juego_back.js
@@ -1,19 +1,19 @@
-async function buscarJuego(idUsuario, nombreJuego) {
+async function buscarJuego(idUsuario, nombreJuego) { //async function
     try {
-        const response = await fetch(`/usuarios/${idUsuario}/juegos/${nombreJuego}`);
+        const response = await fetch(`/usuarios/${idUsuario}/juegos/${nombreJuego}`); //await
         if (!response.ok) {
             return null; // Retorna null si el juego no existe
         }
-        const juego = await response.json();
+        const juego = await response.json(); //await
         return juego;
     } catch (error) {
-        throw new Error(`Error al buscar el juego: ${error.message}`);
+        throw new Error(`Error al buscar el juego: ${error.message}`); //throw
     }
 }
 
-async function crearJuegoParaUsuario(idUsuario, nombreJuego) {
+async function crearJuegoParaUsuario(idUsuario, nombreJuego) { //async function
     try {
-        const response = await fetch(`/usuarios/${idUsuario}/juegos/${nombreJuego}`, {
+        const response = await fetch(`/usuarios/${idUsuario}/juegos/${nombreJuego}`, {//await
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -26,41 +26,41 @@ async function crearJuegoParaUsuario(idUsuario, nombreJuego) {
         });
         
         if (!response.ok) {
-            throw new Error('Error al crear el juego para el usuario');
+            throw new Error('Error al crear el juego para el usuario');//throw
         }
     } catch (error) {
-        throw new Error(`Error al crear el juego para el usuario: ${error.message}`);
+        throw new Error(`Error al crear el juego para el usuario: ${error.message}`);//throw
     }
 }
 
-async function crearNuevaPartida() {
+async function crearNuevaPartida() {//async
     const idUsuario = localStorage.getItem('idUsuario');
-    const nombreJuego = document.getElementById('nombre-juego').textContent.trim();
+    const nombreJuego = document.getElementById('nombre-juego').textContent.trim();//textContent.trim()
 
     try {
         // Verificar si el juego existe para el usuario
-        let juego = await buscarJuego(idUsuario, nombreJuego);
+        let juego = await buscarJuego(idUsuario, nombreJuego);//await
 
         if (!juego) {
             // Si el juego no existe, crearlo
-            await crearJuegoParaUsuario(idUsuario, nombreJuego);
+            await crearJuegoParaUsuario(idUsuario, nombreJuego);//await
            
             // Volver a buscar el juego para obtener la información actualizada
-            juego = await buscarJuego(idUsuario, nombreJuego);
+            juego = await buscarJuego(idUsuario, nombreJuego);//await
         }
       
         // Crear nueva partida
-        const nuevaPartidaResponse = await fetch(`/jugar/${idUsuario}`, {
+        const nuevaPartidaResponse = await fetch(`/jugar/${idUsuario}`, {//await
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ nombreJuego: nombreJuego })
         });
 
         if (!nuevaPartidaResponse.ok) {
-            throw new Error('Error al crear la partida');
+            throw new Error('Error al crear la partida');//throw
         }
 
-        const partida = await nuevaPartidaResponse.json();
+        const partida = await nuevaPartidaResponse.json();//await
         console.log('Partida creada:', partida);
     } catch (error) {
         console.log("LLEGUE AL ERROR")
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,50 +1,50 @@
-document.getElementById('login-form').addEventListener('submit', async function (e) {
+document.getElementById('login-form').addEventListener('submit', async function (e) { //async function
     e.preventDefault();
     
     const nombre = document.getElementById('login-username').value;
     const contraseña = document.getElementById('login-password').value;
     
     try {
-        const response = await fetch('/login', {
+        const response = await fetch('/login', { //await
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ nombre, contraseña })
         });
 
-        const data = await response.json();
+        const data = await response.json(); //await
         
         if (response.status === 200) {
             // Aquí puedes redirigir a la página de juego, estadísticas,etc.
-            localStorage.setItem('idUsuario', data.idUsuario);
+            localStorage.setItem('idUsuario', data.idUsuario); //localStorage
             console.log("Bienvenido")
             window.location.href = './juego/index.html';
         } else {
-            document.getElementById('login-message').textContent = data.message;
+            document.getElementById('login-message').textContent = data.message; //textContent o InnerText?
         }
     } catch (error) {
         console.error('Error:', error);
     }
 });
 
-document.getElementById('signup-form').addEventListener('submit', async function (e) {
+document.getElementById('signup-form').addEventListener('submit', async function (e) { //async function
     e.preventDefault();
     
     const nombre = document.getElementById('signup-username').value;
     const contraseña = document.getElementById('signup-password').value;
     
     try {
-        const response = await fetch('/usuarios', {
+        const response = await fetch('/usuarios', { //await
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ nombre, contraseña })
         });
 
-        const data = await response.json();
+        const data = await response.json(); //await
         
         if (response.status === 201) {
             document.getElementById('signup-message').textContent = 'Usuario creado correctamente.';
         } else {
-            document.getElementById('signup-message').textContent = data.message;
+            document.getElementById('signup-message').textContent = data.message; //textContent o InnerText?
         }
     } catch (error) {
         console.error('Error:', error);
